1b486c9fcc48017a01201dfc7ff93a01584b9f07
Annie <ikram.annie0@gmail.com>
2021-04-24 21:56:07 -0400

    added incomplete code for 4th failure

--- orderBook.js
+++ orderBook.js
@@ -1,38 +1,71 @@
 const reconcileOrder = (existingBook, incomingOrder) => {
-    if (existingBook.length === 0) {
-        existingBook.push(incomingOrder)
-        return existingBook
-    }
+  if (existingBook.length === 0) {
+    existingBook.push(incomingOrder)
 
-    if (possibleDeal(existingBook, incomingOrder) === false) {
-        existingBook.push(incomingOrder)
-        return existingBook
-    }
+    return existingBook
+  }
 
-    if (findDeal(existingBook, incomingOrder) === false) {
-        existingBook.push(incomingOrder)
-        return existingBook
-    }
+  if (possibleDeal(existingBook, incomingOrder) === false) {
+    existingBook.push(incomingOrder)
+
+    return existingBook
+  }
+
+  if (findDeal(existingBook, incomingOrder) === false) {
+    existingBook.push(incomingOrder)
+
+    return existingBook
+  }
+
+  if (fulfillDeal(existingBook, incomingOrder) === false) {
+    existingBook.push(incomingOrder)
+
+    return existingBook
+  }
+
+  if (fulfillDeal2(existingBook, incomingOrder) === false) {
+    return existingBook
+  }
 }
 
-const possibleDeal = (existingBook, incomingOrder ) => {
-    for (let i = 0; i < existingBook.length; i++) {
-        if (existingBook[i].type !== incomingOrder.type) {
-            return true
-        } 
+const possibleDeal = (existingBook, incomingOrder) => {
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].type !== incomingOrder.type) {
+      return true
     }
-            return false
+  }
+
+  return false
 }
 
 const findDeal = (existingBook, incomingOrder) => {
-    for (let i = 0; i < existingBook.length; i++) {
-        if (existingBook[i].type !== incomingOrder.type && (existingBook[i].price === incomingOrder.price)) {
-            return true
-        } 
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].type !== incomingOrder.type && (existingBook[i].price === incomingOrder.price)) {
+      return true
     }
-            return false
+  }
 
+  return false
 }
-   
+
+const fulfillDeal = (existingBook, incomingOrder) => {
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].type !== incomingOrder.type && (existingBook[i].price === incomingOrder.price)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const fulfillDeal2 = (existingBook, incomingOrder) => {
+  for (let i = 0; i < existingBook.length; i++) {
+    if (existingBook[i].type !== incomingOrder.type && (existingBook[i].quantity === incomingOrder.quantity)) {
+      return true
+    }
+  }
+
+  return false
+}
 
 module.exports = reconcileOrder
